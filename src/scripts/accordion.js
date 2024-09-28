3fa7c2b3b6e2b9e3421a8e32caba1a66a96b8d7e
mbibko <mbibko@gmail.com>
2024-09-28 16:00:18 +0300

    fixed js error on entry pages

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,11 +1,14 @@
 import Accordion from 'accordion-js';
+
 let accordionWrapperElement = document.querySelector(".tailwind-accordion-container");
-let accordionWrapperNestedElements = accordionWrapperElement.querySelectorAll(".tailwind-accordion-container-nested");
+if (accordionWrapperElement) {
+  let accordionWrapperNestedElements = accordionWrapperElement.querySelectorAll(".tailwind-accordion-container-nested");
 
 
-new Accordion([accordionWrapperElement, [...accordionWrapperNestedElements]], {
-  elementClass: "tailwind-ac",
-  triggerClass: "tailwind-trigger",
-  panelClass: "tailwind-panel",
-  activeClass: "tailwind-active"
-});
+  new Accordion([accordionWrapperElement, [...accordionWrapperNestedElements]], {
+    elementClass: "tailwind-ac",
+    triggerClass: "tailwind-trigger",
+    panelClass: "tailwind-panel",
+    activeClass: "tailwind-active"
+  });
+}
